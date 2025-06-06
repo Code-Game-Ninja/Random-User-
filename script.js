17637c6dd1b617c5271076037759a1ca90e994b5
Code-Game-Ninja <chiragmishra573@gmail.com>
2025-06-06 21:49:30 +0530

    Merge remote main with local

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 async function fetchRandomUser() {
-    const response = await fetch('https://randomuser.me/api/');
-    const data = await response.json();
-    const user = data.results[0];
-  
-    document.querySelector('.user').innerHTML = `
+  const response = await fetch('https://randomuser.me/api/');
+  const data = await response.json();
+  const user = data.results[0];
+
+  document.querySelector('.user').innerHTML = `
       <img src="${user.picture.large}" alt="User Picture" />
       <h2>${user.name.first} ${user.name.last}</h2>
       <p><strong>Email:</strong> ${user.email}</p>
@@ -12,10 +12,10 @@ async function fetchRandomUser() {
       <p><strong>Username:</strong> ${user.login.username}</p>
       <p><strong>Age:</strong> ${user.dob.age}</p>
     `;
-  }
-  document.querySelector('.get').addEventListener('click', fetchRandomUser);
-  
-    // Initial fetch to display a random user on page load
-    // If you want to fetch a random user when the page loads, uncomment the line below:
+}
+document.querySelector('.get').addEventListener('click', fetchRandomUser);
 
-    //  fetchRandomUser();
+// Initial fetch to display a random user on page load
+// If you want to fetch a random user when the page loads, uncomment the line below:
+
+//  fetchRandomUser();
